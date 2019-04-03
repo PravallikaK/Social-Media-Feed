cccdd34d233bce95f7c624fc952addce3c1159c7
pravallika kolukuluri <pravallika.kolukuluri@walmartlabs.com>
2019-04-03 10:14:57 -0700

    work for social media feed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './App.css';
+import FeedInfo from "./FeedInfo";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+        feeds: [],
+        feed: {
+          title: '',
+          url: ''
+        }
+    }
+    this.updateFeed = this.updateFeed.bind(this);
+    this.addFeed = this.addFeed.bind(this);
+  }
+  updateFeed = (e) => {
+    const { feed } = this.state;
+    this.setState({
+        feed: {
+            ...feed,
+            [e.target.name]: e.target.value
+        }
+    });
+  }
+  addFeed = (event) => {
+    const { feeds, feed } = this.state;
+  if(feed.title !== '' && feed.url !== ''){
+    this.setState({
+        feeds: [...feeds, feed]
+    });
+  }
+    event.preventDefault();
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  render() {
+    const { feeds } = this.state;
+    return (
+      <div className="App">
+        <section id="search" className="alt" style={{ display: "block" }}>
+            <form method="post" action="#">
+                <input onChange={this.updateFeed} name="url" type="text" id= "url" placeholder="Enter URL" /><br />
+                <input onChange={this.updateFeed} name="title" type="text" id= "title" placeholder="Title" />
+                <button onClick={this.addFeed} id="mybutton" >New Post</button>
+            </form>
+        </section>
+        {feeds.length >0 && (<FeedInfo feeds= {feeds} />  )}
+      </div>
+    ); 
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));  
